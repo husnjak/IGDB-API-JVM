7813b3dd2365d0e89fee72e2ad4ed951e62e3b09
Husnjak <husnjak@twitch.tv>
2023-05-22 13:52:20 +0200

    Update Tests

--- src/test/kotlin/com/api/igdb/TestTwitchTokenKotlin.kt
+++ src/test/kotlin/com/api/igdb/TestTwitchTokenKotlin.kt
@@ -2,6 +2,7 @@ package com.api.igdb
 
 import com.api.igdb.request.TwitchAuthenticator
 import org.junit.jupiter.api.Test
+import java.time.Duration
 
 class TestTwitchTokenKotlin {
     @Test
@@ -10,8 +11,8 @@ class TestTwitchTokenKotlin {
         val token = TwitchAuthenticator.requestTwitchToken(System.getenv("client_id"), System.getenv("client_secret"))
 
         assert(token != null)
-        assert(token!!.expires_in > 5000000)
-        val tokenFutureDate = System.currentTimeMillis() + 5000000 // 57 days
+        assert(token!!.expires_in > Duration.ofDays(60).toSeconds())
+        val tokenFutureDate = System.currentTimeMillis() + Duration.ofDays(60).toSeconds()
         assert(tokenFutureDate > token.getExpiresUnix())
     }
 }
